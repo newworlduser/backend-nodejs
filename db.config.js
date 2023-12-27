4aa0b53a47d6d1163749e2f12e5077145f4a9bab
ChampionOverlord <devesh.random18@gmail.com>
2023-12-27 11:21:26 +0530

    modified:   db.config.js

--- db.config.js
+++ db.config.js
@@ -2,10 +2,12 @@
 const mongoose = require('mongoose');
 const uri=`mongodb+srv://${process.env.DBUSER}:${process.env.PASS}@cluster0.iqb3ny3.mongodb.net/?retryWrites=true&w=majority`
 
-
+exports.dbConfig=()=>{
 mongoose.connect(uri).then(()=>{
     console.log('connected to db...')
 }).catch((err)=>{
+    console.log(process.env.PASS)
     console.error(`Error connecting to the db \n${err}`)
 
 })
+}
