d26bb0124fe4fb21e6c3f44bfb2e30ff822ce27b
ChampionOverlord <devesh.random18@gmail.com>
2023-12-27 12:37:02 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const db = require('./db.config')
 const PORT = process.env.PORT || 5000;
+//controller import
 const userRouter = require('./controller/user.controller')
 
 
