476c9ef568e465f3e660eeecb289f025817a288d
ChampionOverlord <devesh.random18@gmail.com>
2023-12-28 11:25:28 +0530

    modified:   controller/user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const User=require('../models/user.model')
+const User = require('../models/user.model')
 
 
-router.post("/",(req,res,next)=>{
-    res.json(req.body)
-res.json('data receiverd...')
-
+router.post("/",(req,res)=>{
+    res.json(req.body);
+    // res.json('data received...');
 })
 
 
@@ -14,24 +13,18 @@ router.get('/',(req,res,next)=>{
     res.json("get all users information")
 })
 
-router.get("/:id",(req,res,next)=>{
+router.get('/:id',(req,res,next)=>{
     res.json("get user information")
 })
-
 
 router.delete("/:id",(req,res,next)=>{
     res.json("delete user information")
 })
 
-router.delete("/",(req,res,next)=>{
-    res.json("delete all users information")
-})
 
-router.post("/",(req,res,next)=>{
-    res.json("insert user information")
-})
 
 router.put("/:id",(req,res,next)=>{
     res.json("update user information")
 })
-module.exports=router;
+
+module.exports = router;
