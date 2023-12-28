55db86d13c897d7f10ce2acc1a2b8d71cc55333f
ChampionOverlord <devesh.random18@gmail.com>
2023-12-28 10:59:21 +0530

    modified:   models/user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-const userSchema={
+const userSchema=mongoose.Schema(
+    {
     firstName:{type: String},
     lastName:{type: String},
     dept:{type: String},
     age:{type: Number}
-};
+    },
+     {timestamp:true}
+);
 module.exports=mongoose.model('User',userSchema)
